5c37d7e5ce2f254a2a3f11732d712ffca44e2798
Tyron Spencer <olijoapps@gmail.com>
2017-08-26 06:19:19 +1000

    login view completed

---
+++ Smack/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Tyron Spencer on 25/8/17.
+//  Copyright © 2017 Tyron Spencer. All rights reserved.
+//
+
+import Foundation
+
+// Segues:
+let TO_LOGIN = "toLogin"
---
+++ Smack/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Tyron Spencer on 26/8/17.
+//  Copyright © 2017 Tyron Spencer. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
